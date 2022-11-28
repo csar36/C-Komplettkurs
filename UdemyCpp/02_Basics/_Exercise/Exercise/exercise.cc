643cdab5d8351a9cad42b0644c11c2c7ee34755f
csar36 <c_sar36@hotmail.com>
2022-11-28 18:54:16 +0100

    solving first exercise

--- UdemyCpp/02_Basics/_Exercise/Exercise/exercise.cc
+++ UdemyCpp/02_Basics/_Exercise/Exercise/exercise.cc
@@ -4,4 +4,20 @@
 
 void mod_cross_sum(int I, int J)
 {
+    for(int i = 0; i< I ;i++)
+    {
+        for(int j=0; j<J;j++)
+        {
+            if((i+j) % 2 ==0)
+            {
+                std::cout << "Für i = " << i << " und j = " << j << " | gerade" << std::endl;
+            }
+            else if((i+j) % 2 !=0)
+            {
+                std::cout << "Für i = " << i << " und j = " << j << " | ungerade" << std::endl;
+            }
+
+        }
+    }
+
 }
